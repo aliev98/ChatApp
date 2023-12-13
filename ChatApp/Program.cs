d29f349e4fec074321b0b8d4424ecec29971d4ed
Ali Elias <kuba-98@hotmail.com>
2023-12-13 21:25:54 +0100

    Initial commit

---
+++ ChatApp/Program.cs
@@ -0,0 +1,36 @@
+
+using ChatApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+var app = builder.Build();
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapHub<ChatHub>("/Hub/ChatHub");
+//});
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/chatHub");
+app.Run();
